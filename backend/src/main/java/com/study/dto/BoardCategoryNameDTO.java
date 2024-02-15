fbc8bc36871b3e75a9cc4ce8b96e5c8d01e5da4f
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-16 02:51:08 +0900

    feat : board with categoryName DTO

---
+++ backend/src/main/java/com/study/dto/BoardCategoryNameDTO.java
@@ -0,0 +1,25 @@
+package com.study.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+/**
+ * 게시판 목록에 필요한 정보들
+ */
+@Getter
+@Setter
+@Builder
+public class BoardCategoryNameDTO {
+    private String categoryName;
+    private Long boardId;
+    private String title;
+    private String userName;
+    private Long views;
+    private Timestamp createdAt;
+    private Timestamp editedAt;
+}
+
+
