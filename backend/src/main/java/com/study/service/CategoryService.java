8e4910634c259670f74b727f6774e2d915f1319d
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-16 02:53:43 +0900

    feat : category service

---
+++ backend/src/main/java/com/study/service/CategoryService.java
@@ -0,0 +1,38 @@
+package com.study.service;
+
+import com.study.dto.CategoryDTO;
+import com.study.mapper.CategoryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 카테고리 관련 비지니스 로직
+ */
+@Service
+public class CategoryService {
+    private final CategoryMapper categoryMapper;
+
+    @Autowired
+    public CategoryService(CategoryMapper categoryMapper){
+        this.categoryMapper = categoryMapper;
+    }
+
+    /**
+     * DB에 있는 Category 전부 가져오기
+     * @return
+     */
+    public List<CategoryDTO> getCategoryList(){
+        return categoryMapper.getCategoryList();
+    }
+
+    /**
+     * pk로 Category 하나 가져오기
+     * @param categoryId
+     * @return
+     */
+    public CategoryDTO getCategory(Long categoryId){
+        return categoryMapper.findById(categoryId);
+    }
+}
