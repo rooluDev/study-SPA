20791404773f3c0d48e3241f9637709757953b66
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-16 02:52:58 +0900

    feat : Board DB select condition

---
+++ backend/src/main/java/com/study/condition/BoardSelectCondition.java
@@ -0,0 +1,22 @@
+package com.study.condition;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+/**
+ * DB에서 board 검색 조건
+ */
+@Getter
+@Setter
+@Builder
+public class BoardSelectCondition {
+    private Timestamp startDate;
+    private Timestamp endDate;
+    private int categoryId;
+    private String searchText;
+    private int pageSize;
+    private int startRow;
+}
