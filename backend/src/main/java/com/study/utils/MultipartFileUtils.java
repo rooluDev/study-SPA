e562fb9f3fb20e6085b39dfb26bdad3a5f0b5c38
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-16 02:56:46 +0900

    feat : MultiPart File Util

---
+++ backend/src/main/java/com/study/utils/MultipartFileUtils.java
@@ -0,0 +1,22 @@
+package com.study.utils;
+
+/**
+ * MultipartFile Utils
+ */
+public class MultipartFileUtils {
+
+    /**
+     * 이름에서 확장자 추출
+     * @param originalFileName
+     * @return
+     */
+    public static String extractExtension(String originalFileName){
+        if (originalFileName != null) {
+            int lastDotIndex = originalFileName.lastIndexOf('.');
+            if (lastDotIndex > 0) {
+                return originalFileName.substring(lastDotIndex + 1);
+            }
+        }
+        return null;
+    }
+}
