8a5e34593b8ae125b7c783fbd698e85419b548a0
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-16 02:45:51 +0900

    feat : router 설정

---
+++ frontend/src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import List from '@/pages/boards/free/list/ListBoard.vue';
+import Write from '@/pages/board/free/write/WriteBoard.vue';
+import Modify from '@/pages/board/free/modify/ModifyBoard.vue';
+import View from '@/pages/boards/free/view/ViewBoard.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/boards/free/list',
+      name: 'List',
+      component: List,
+    },
+    {
+      path: '/board/free/write',
+      name: 'Write',
+      component: Write,
+    },
+    {
+      path: '/board/free/modify',
+      name: 'Modify',
+      component: Modify,
+    },
+    {
+      path: '/boards/free/view/:id',
+      name: 'View',
+      component: View,
+    },
+  ],
+});
+
+export default router;
