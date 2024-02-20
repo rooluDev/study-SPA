032c94113239092d4d46e1a995b73e8af248466c
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-20 21:13:28 +0900

    feat : throws Exception

--- backend/src/main/java/com/study/controller/CategoryController.java
+++ backend/src/main/java/com/study/controller/CategoryController.java
@@ -1,7 +1,7 @@
 package com.study.controller;
 
 
-import com.study.dto.CategoryDTO;
+import com.study.dto.CategoryDto;
 import com.study.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,32 +24,28 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @Autowired
-    public CategoryController(CategoryService categoryService){
+    public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
     /**
      * 모든 카테고리 리스트 요청
+     *
      * @return
      */
     @GetMapping("/categories")
-    public ResponseEntity<List<CategoryDTO>> getCategoryList(){
-        return new ResponseEntity<>(categoryService.getCategoryList(), HttpStatus.OK);
+    public ResponseEntity<List<CategoryDto>> getCategoryList() {
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.getCategoryList());
     }
 
     /**
      * 단일 카테고리 요청
+     *
      * @param categoryId
      * @return
      */
     @GetMapping("/category/{categoryId}")
-    public ResponseEntity<CategoryDTO> getCategory(@PathVariable Long categoryId){
-        try {
-            CategoryDTO categoryDTO = categoryService.getCategory(categoryId);
-            return new ResponseEntity<>(categoryDTO,HttpStatus.OK);
-        }catch (Exception e){
-            log.error(e.getMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<CategoryDto> getCategory(@PathVariable Long categoryId) throws Exception {
+        return ResponseEntity.ok().body(categoryService.findById(categoryId));
     }
 }
