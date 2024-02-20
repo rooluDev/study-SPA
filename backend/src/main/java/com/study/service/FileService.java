067122c2951a73e9f05d269c61f961e0688ab1b9
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-20 21:06:55 +0900

    feat : FileServerRepository 멤버변수 추가

--- backend/src/main/java/com/study/service/FileService.java
+++ backend/src/main/java/com/study/service/FileService.java
@@ -1,7 +1,9 @@
 package com.study.service;
 
-import com.study.dto.FileDTO;
+import com.study.dto.FileDto;
+import com.study.exception.FileNotFoundException;
 import com.study.mapper.FileMapper;
+import com.study.repository.FileServerRepository;
 import com.study.utils.MultipartFileUtils;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.*;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * 파일 관련 비지니스 로직
@@ -19,12 +22,14 @@ import java.util.UUID;
 @Service
 public class FileService {
     @Value("#{file['file.path']}")
-    private String REAL_PATH;
+    private String path;
     private final FileMapper fileMapper;
+    private final FileServerRepository fileServerRepository;
 
     @Autowired
-    public FileService(FileMapper fileMapper) {
+    public FileService(FileMapper fileMapper, FileServerRepository fileServerRepository) {
         this.fileMapper = fileMapper;
+        this.fileServerRepository = fileServerRepository;
     }
 
     /**
@@ -32,8 +37,13 @@ public class FileService {
      * @param boardId
      * @return
      */
-    public List<FileDTO> getFiles(Long boardId){
-        return fileMapper.findByBoardId(boardId);
+    public List<FileDto> findFileListByBoardId(Long boardId){
+        // Entity -> Dto
+        List<FileDto> fileDtoList = fileMapper.findByBoardId(boardId).stream()
+                .map(entity -> entity.toFileDto())
+                .collect(Collectors.toList());
+
+        return fileDtoList;
     }
 
     /**
@@ -42,24 +52,22 @@ public class FileService {
      * @param multipartFiles
      * @param boardId
      */
-    public void uploadFile(MultipartFile[] multipartFiles, Long boardId) throws IOException {
+    public void addFile(MultipartFile[] multipartFiles, Long boardId) throws IOException {
         if(multipartFiles != null) {
             for (MultipartFile multipartFile : multipartFiles) {
                 if (!multipartFile.isEmpty()) {
                     // File DTO 생성
-                    FileDTO file = FileDTO.builder()
+                    com.study.entity.File file = com.study.entity.File.builder()
                             .boardId(boardId)
                             .originalName(multipartFile.getOriginalFilename())
                             .physicalName(UUID.randomUUID().toString())
-                            .filePath(REAL_PATH)
+                            .filePath(path)
                             .extension(MultipartFileUtils.extractExtension(multipartFile.getOriginalFilename()))
                             .size(multipartFile.getSize())
                             .build();
 
                     // Server 저장
-                    String filePath = REAL_PATH + file.getPhysicalName() + "." + file.getExtension();
-                    File uploadedFile = new File(filePath);
-                    FileUtils.copyInputStreamToFile(multipartFile.getInputStream(), uploadedFile);
+                    fileServerRepository.createFile(file.toFileDto(),multipartFile);
 
                     // File DB Add
                     fileMapper.createFile(file);
@@ -72,16 +80,16 @@ public class FileService {
      * 게시물의 등록된 파일 DB에서 삭제
      * @param boardId
      */
-    public void deleteFiles(Long boardId){
-        fileMapper.deleteByBoardId(boardId);
+    public int deleteFilesByBoardId(Long boardId){
+        return fileMapper.deleteByBoardId(boardId);
     }
 
     /**
      * 하나의 파일 삭제
      * @param fileId
      */
-    public void deleteFileById(Long fileId){
-        fileMapper.deleteById(fileId);
+    public int deleteFileByFileId(Long fileId){
+        return fileMapper.deleteById(fileId);
     }
 
     /**
@@ -89,11 +97,7 @@ public class FileService {
      * @param fileId
      * @return
      */
-    public FileDTO findOne(Long fileId) throws Exception{
-        FileDTO fileDTO = fileMapper.findById(fileId);
-        if(fileDTO == null){
-            throw new FileNotFoundException("No file in DB");
-        }
-        return fileDTO;
+    public FileDto findByFileId(Long fileId) throws FileNotFoundException {
+        return fileMapper.findByFileId(fileId).orElseThrow(() -> new com.study.exception.FileNotFoundException()).toFileDto();
     }
 }
