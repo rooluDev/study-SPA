369318052efd331cddf6b0aa9afb4623bc2828fb
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-20 21:11:48 +0900

    refactor : extends RuntimeException

--- backend/src/main/java/com/study/exception/CategoryNotFoundException.java
+++ backend/src/main/java/com/study/exception/CategoryNotFoundException.java
@@ -3,7 +3,7 @@ package com.study.exception;
 /**
  * 잘못된 category pk로 category를 select할 때
  */
-public class CategoryNotFoundException extends Exception{
+public class CategoryNotFoundException extends RuntimeException{
     public CategoryNotFoundException() {
     }
 
