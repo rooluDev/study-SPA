42c9a965e2bf1151292cf8e9fd53d230ca52fa83
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-20 21:12:43 +0900

    refactor : Entity 사용

--- backend/src/main/java/com/study/mapper/CategoryMapper.java
+++ backend/src/main/java/com/study/mapper/CategoryMapper.java
@@ -1,9 +1,10 @@
 package com.study.mapper;
 
-import com.study.dto.CategoryDTO;
+import com.study.entity.Category;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Category Mapper
@@ -14,12 +15,12 @@ public interface CategoryMapper {
      * DB에 있는 모든 카테고리 리스트 검색
      * @return
      */
-    List<CategoryDTO> getCategoryList();
+    List<Category> getCategoryList();
 
     /**
      * pk로 카테고리 검색
      * @param categoryId
      * @return
      */
-    CategoryDTO findById(Long categoryId);
+    Optional<Category> findById(Long categoryId);
 }
