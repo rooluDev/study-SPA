3113bdfe759e7c365705fafe957f54ed8e1e7928
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-20 21:10:10 +0900

    refactor : Stream 으로 Comment Dto변경

--- backend/src/main/java/com/study/service/CommentService.java
+++ backend/src/main/java/com/study/service/CommentService.java
@@ -1,11 +1,14 @@
 package com.study.service;
 
-import com.study.dto.CommentDTO;
+import com.study.dto.CommentCreateFormDto;
+import com.study.dto.CommentDto;
+import com.study.entity.Comment;
 import com.study.mapper.CommentMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 댓글 관련 비지니스 로직
@@ -24,23 +27,26 @@ public class CommentService {
      * @param boardId
      * @return
      */
-    public List<CommentDTO> getComments(Long boardId){
-        return commentMapper.findByBoardId(boardId);
+    public List<CommentDto> getComments(Long boardId){
+        // Entity -> DTO
+        List<CommentDto> commentDtoList = commentMapper.findByBoardId(boardId).stream()
+                .map(entity -> entity.toCommentDto())
+                .collect(Collectors.toList());
+        return commentDtoList;
     }
 
     /**
      * 댓글 등록
-     * @param boardId
-     * @param comment
+     * @param commentCreateFormDto
      */
-    public void registerComment(Long boardId, String comment){
-        commentMapper.createComment(boardId, comment);
+    public int addComment(CommentCreateFormDto commentCreateFormDto){
+        return commentMapper.createComment(commentCreateFormDto);
     }
 
     /**
      * 댓글 모두 삭제
      */
-    public void deleteByBoardId(Long boardId){
-        commentMapper.deleteByBoardId(boardId);
+    public int deleteByBoardId(Long boardId){
+        return commentMapper.deleteByBoardId(boardId);
     }
 }
