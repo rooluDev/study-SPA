d701b4a24ab51c72a1361240554aa8a405712ef9
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-20 21:02:07 +0900

    feat : 게시물 수정 Form Dto

---
+++ backend/src/main/java/com/study/dto/BoardUpdateFormDto.java
@@ -0,0 +1,13 @@
+package com.study.dto;
+
+import lombok.Getter;
+
+/**
+ * 게시물 수정 form data
+ */
+@Getter
+public class BoardUpdateFormDto {
+    private String userName;
+    private String title;
+    private String content;
+}
