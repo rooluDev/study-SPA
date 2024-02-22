196171b0a9b06b1a0a07d9962b3b2fd77620b98d
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-23 02:24:46 +0900

    feat : ErrorResponse Entity
    {
       status: "",
       errorCode:"" ,
       message:""
    }

---
+++ backend/src/main/java/com/study/exception/common/ErrorResponse.java
@@ -0,0 +1,27 @@
+package com.study.exception.common;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Error 응답 Class
+ */
+@Getter
+@NoArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String errorCode;
+    private String resultMessage;
+
+    @Builder
+    protected ErrorResponse(final ErrorCode errorCode){
+        this.status = errorCode.getStatus();
+        this.errorCode = errorCode.getErrorCode();
+        this.resultMessage = errorCode.getMessage();
+    }
+
+    public static ErrorResponse of(final ErrorCode code){
+        return new ErrorResponse(code);
+    }
+}
