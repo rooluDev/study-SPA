cc262233297600da12bbbafa7130e09eddb24141
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-23 02:25:55 +0900

    refactor : Setter추가

--- backend/src/main/java/com/study/dto/BoardCreateFormDto.java
+++ backend/src/main/java/com/study/dto/BoardCreateFormDto.java
@@ -1,9 +1,11 @@
 package com.study.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
+@Setter
 public class BoardCreateFormDto {
     private MultipartFile[] files;
     private Long categoryId;
