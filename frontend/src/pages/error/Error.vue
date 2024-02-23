623a82f2394164798d8d9da1d41f50c6ac164600
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-24 00:41:12 +0900

    feat : Error Page

---
+++ frontend/src/pages/error/Error.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Error</h1>
+  <button @click="goToList">List</button>
+</template>
+
+<script>
+import { useRouter } from 'vue-router';
+
+export default {
+  setup() {
+    const router = useRouter();
+    const goToList = () => {
+      router.push({
+        name: 'List',
+      });
+    };
+    return {
+      goToList,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
