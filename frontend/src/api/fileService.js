0a229d58c87b614ebc7e8a90fe1010ecc06c5fdc
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-25 00:47:47 +0900

    docs : 주석 추가

--- frontend/src/api/fileService.js
+++ frontend/src/api/fileService.js
@@ -1,19 +1,36 @@
 import axios from 'axios';
 
+/**
+ * GET /api/file/fileId/download
+ *
+ * @param fileId
+ * @returns {Promise<any>}
+ */
 export async function downloadFile(fileId) {
   const res = await axios.get(`/api/file/${fileId}/download`, {
     responseType: 'blob',
   });
-
   return res.data;
 }
 
+/**
+ * GET /api/files/boardId
+ *
+ * @param boardId
+ * @returns {Promise<*>}
+ */
 export async function getFileList(boardId) {
   const res = await axios.get(`/api/files/${boardId}`);
-  return res.data;
+  return res.data.body;
 }
 
+/**
+ * DELETE /api/file/fileId
+ *
+ * @param fileId
+ * @returns {Promise<*>}
+ */
 export async function deleteFile(fileId) {
   const res = await axios.delete(`/api/file/${fileId}`);
-  return res.data;
+  return res.data.body;
 }
