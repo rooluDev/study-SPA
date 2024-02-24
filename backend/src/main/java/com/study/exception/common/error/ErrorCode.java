a8607adebfc36993f29fab3eebdde1e632c44a2d
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-25 00:38:45 +0900

    rename : error package 추가

--- backend/src/main/java/com/study/exception/common/error/ErrorCode.java
+++ backend/src/main/java/com/study/exception/common/error/ErrorCode.java
@@ -1,4 +1,4 @@
-package com.study.exception.common;
+package com.study.exception.common.error;
 
 import lombok.Getter;
 
@@ -8,17 +8,17 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     // @RequestBody 데이터 미 존재
-    REQUEST_BODY_MISSING_ERROR(400, "A001", "Required request body is missing"),
+    REQUEST_BODY_MISSING_ERROR(404, "A001", "Required request body is missing"),
     // Request Parameter 로 데이터가 전달되지 않을 경우
-    MISSING_REQUEST_PARAMETER_ERROR(400, "A002", "Missing Servlet RequestParameter Exception"),
+    MISSING_REQUEST_PARAMETER_ERROR(404, "A002", "Missing Servlet RequestParameter Exception"),
     // SQL Exception 전체 처리
-    SQL_ERROR(404,"A003","SQL_Exception"),
+    SQL_ERROR(500,"A003","SQL_Exception"),
     // IO Exception 전체 처리
-    IO_ERROR(400, "A004", "I/O Exception"),
+    IO_ERROR(500, "A004", "I/O Exception"),
     // NULL Point Exception 발생
-    NULL_POINT_ERROR(404, "A005", "Null Point Exception"),
+    NULL_POINT_ERROR(500, "A005", "Null Point Exception"),
     // @RequestBody 및 @RequestParam, @PathVariable 값이 유효하지 않음
-    NOT_VALID_ERROR(404, "A006", "handle Validation Exception"),
+    NOT_VALID_ERROR(400, "A006", "handle Validation Exception"),
     // 서버가 처리 할 방법을 모르는 경우 발생
     INTERNAL_SERVER_ERROR(500, "A007", "Internal Server Error Exception"),
     // 존재하지 않은 pk로 board 조회시
@@ -32,9 +32,9 @@ public enum ErrorCode {
     // 카테고리 선택이 안되어있을 때
     NO_SELECTED_ERROR(404,"A012","No Category Selected Exception"),
     // password, passwordCheck 값이 일치하지 않을 때
-    PASSWORD_INCORRECT_ERROR(403,"A013","Password Incorrect Exception"),
+    PASSWORD_INCORRECT_ERROR(400,"A013","Password Incorrect Exception"),
     // password가 정책상의 규정과 맞지 않을 때
-    PASSWORD_REGEX_ERROR(403,"A014","Password Regex Exception");
+    PASSWORD_REGEX_ERROR(400,"A014","Password Regex Exception");
 
 
 
--- backend/src/main/java/com/study/exception/common/error/ErrorResponse.java
+++ backend/src/main/java/com/study/exception/common/error/ErrorResponse.java
@@ -1,4 +1,4 @@
-package com.study.exception.common;
+package com.study.exception.common.error;
 
 import lombok.Builder;
 import lombok.Getter;
