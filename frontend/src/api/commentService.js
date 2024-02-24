f982f0fdd23d45a28fd664969e63b38ccc1c369d
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-25 00:47:42 +0900

    docs : 주석 추가

--- frontend/src/api/commentService.js
+++ frontend/src/api/commentService.js
@@ -1,14 +1,25 @@
 import axios from 'axios';
 
+/**
+ * GET /api/comment/boardId
+ * @param boardId
+ * @returns {Promise<*>}
+ */
 export async function getCommentList(boardId) {
   const res = await axios.get(`/api/comment/${boardId}`);
-  return res.data;
+  return res.data.body;
 }
 
+/**
+ * POST /api/comment
+ * @param boardId
+ * @param comment
+ * @returns {Promise<*>}
+ */
 export async function createComment(boardId, comment) {
   const res = await axios.post(`/api/comment`, {
     boardId: boardId,
     comment: comment,
   });
-  return res.data;
+  return res.data.body;
 }
