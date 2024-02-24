12a081b0657a0a2b6116626d64a118439179256b
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-25 00:48:02 +0900

    feat : Error router 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import List from '@/pages/boards/free/list/ListBoard.vue';
 import Write from '@/pages/board/free/write/WriteBoard.vue';
 import Modify from '@/pages/board/free/modify/ModifyBoard.vue';
 import View from '@/pages/boards/free/view/ViewBoard.vue';
+import Error from '@/pages/error/Error.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: '/boards/free/view/:id',
       name: 'View',
       component: View,
+    },
+    {
+      path: '/error',
+      name: 'Error',
+      component: Error,
     },
   ],
 });
