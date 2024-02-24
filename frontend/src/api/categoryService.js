2b2bf99b1ba1b9d0cf94137b3ee6fe954856f6d1
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-02-25 00:47:29 +0900

    refactor : getCategoryById 삭제

--- frontend/src/api/categoryService.js
+++ frontend/src/api/categoryService.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
+/**
+ * GET /api/categories
+ * @returns {Promise<*>}
+ */
 export async function getCategoryList() {
   const res = await axios.get(`/api/categories`);
-  return res.data;
-}
-
-export async function getCategoryById(categoryId) {
-  const res = await axios.get(`/api/category/${categoryId}`);
-  return res.data;
+  return res.data.body;
 }
